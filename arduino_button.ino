a9b3ed5af9d62acbb3b268703a81cf73122575e4
Luc <l.schootuiterkamp@utwente.nl>
2023-07-26 10:56:49 +0200

    button working

--- arduino_button.ino
+++ arduino_button.ino
@@ -4,48 +4,51 @@
  */
 
 #include <ros.h>
-#include <std_msgs/Int16MultiArray.h>
+// #include "/home/luc/catkin_ws/pacof_whack_a_mole/devel/include/wham/ledlist.h"
+#include <wham/ledlist.h>
+#include <wham/buttonlist.h>
+
+#include <std_msgs/Empty.h>
 
 ros::NodeHandle  nh;
+int ledpins[16] = {52,50,48,46,44,42,40,38,36,34,32,30,28,26,24,22};
+int buttonpins[16] =  {53,51,49,47,45,43,41,39,37,35,33,31,29,27,25,23};
 
-int ledlist[] = {52,50,48,46,44,42,40,38,36,34,32,30,28,26,24,22};
-int buttonlist[] =  {53,51,49,47,45,43,41,39,37,35,33,31,29,27,25,23};
-std_msgs::Int16MultiArray button_state;
-int i;
-
-
-void updateLeds(const std_msgs::Int16MultiArray& leds){
+void updateLeds(wham::ledlist* leds){
   // leds and ledlist should be the same length
-  for (i=0;i<sizeof(leds);i++){
-    digitalWrite(ledlist[i],leds.data[i]);
+  for (int i=0;i<16;i++){
+    digitalWrite(ledpins[i],leds->leds[i]);
   }
 }
+ros::Subscriber<wham::ledlist> ledstate("ledstate", updateLeds); 
 
-ros::Subscriber<std_msgs::Int16MultiArray> ledstate("ledstate", 1000, updateLeds); 
-
-ros::Publisher buttonstate("buttonstate",1000, &button_state);
+wham::buttonlist button_state;
+ros::Publisher button_pub("buttonstate", &button_state);
 
 void setup()
 {
-  for (i=0;i<sizeof(ledlist);i++){
-    pinMode(i, OUTPUT);
+  for (int i=0;i<16;i++){
+    pinMode(ledpins[i], OUTPUT);
   }
-  for (i=0;i<sizeof(buttonlist);i++){
-    pinMode(i, INPUT);
+  for (int i=0;i<16;i++){
+    pinMode(buttonpins[i], INPUT_PULLUP);
   }
 
   nh.initNode();
-  nh.advertise(buttonstate);
+  
+  nh.advertise(button_pub);
+ 
   nh.subscribe(ledstate);
+  nh.negotiateTopics();
+  
 }
 
 void loop()
 {
-  for (i=0;i<sizeof(buttonlist);i++){
-    button_state.data[i]=digitalRead(i);
+  for (int i=0;i<16;i++){
+    button_state.buttons[i]= digitalRead(buttonpins[i]);
   }
-  
-  buttonstate.publish( &button_state );
+  button_pub.publish(&button_state); // publish the adress of button_state..?
   nh.spinOnce();
-  delay(500);
+  delay(5);
 }
